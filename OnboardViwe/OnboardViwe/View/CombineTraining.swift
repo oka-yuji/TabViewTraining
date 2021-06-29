2657edebaf0b20320e6bc907f9822462d6ba00e7
岡優志 <yu.j.0513@gmail.com>
2021-06-29 21:02:25 +0900

    ImageのURLを変更

--- OnboardViwe/OnboardViwe/View/CombineTraining.swift
+++ OnboardViwe/OnboardViwe/View/CombineTraining.swift
@@ -9,32 +9,42 @@ import SwiftUI
 import Combine
 
 struct CombineTraining: View {
+    @EnvironmentObject var webModel: WebModel
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            self.webModel.dlImage != nil ? Image(uiImage: self.webModel.dlImage!).resizable()
+                : Image(systemName: "house")
+        }
+        .aspectRatio(contentMode: .fit)
+        .frame(width: 300, height: 300)
+        .onAppear(perform: {
+            self.webModel.fetch()
+        })
     }
 }
 
 struct CombineTraining_Previews: PreviewProvider {
     static var previews: some View {
         CombineTraining()
+            .environmentObject(WebModel())
     }
 }
 
 
 class WebModel: ObservableObject {
-  @Published var dlImage: UIImage?
-  private var cancellableNetwork: AnyCancellable? = nil
-  let session = URLSession.shared
-
-  /// 画像の受信
-  func fetch() {
-     let url = URL(string: "https://upload.wikimedia.org/wikipedia/commons/1/1a/Yukichi_Fukuzawa_1891.jpg")!
-
-     cancellableNetwork = session
-        .dataTaskPublisher(for: url)
-        .map { data, URLResponse in UIImage(data: data) }
-        .receive(on: DispatchQueue.main)
-        .replaceError(with: nil)
-        .assign(to: \.dlImage, on: self)
-  }
+    @Published var dlImage: UIImage?
+    private var cancellableNetwork: AnyCancellable? = nil
+    let session = URLSession.shared
+    
+    /// 画像の受信
+    func fetch() {
+        let url = URL(string: "https://illustimage.com/photo/6845.png")!
+        
+        cancellableNetwork = session
+            .dataTaskPublisher(for: url)
+            .map { data, URLResponse in UIImage(data: data) }
+            .receive(on: DispatchQueue.main)
+            .replaceError(with: nil)
+            .assign(to: \.dlImage, on: self)
+    }
 }
