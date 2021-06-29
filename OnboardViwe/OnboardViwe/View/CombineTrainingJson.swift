83de3bdeb635b4d8b3512883899e6a91186a1032
岡優志 <yu.j.0513@gmail.com>
2021-06-29 22:22:55 +0900

    CombineTrainingJsonを追加

---
+++ OnboardViwe/OnboardViwe/View/CombineTrainingJson.swift
@@ -0,0 +1,20 @@
+//
+//  CombineTrainingJson.swift
+//  OnboardViwe
+//
+//  Created by 岡優志 on 2021/06/29.
+//
+
+import SwiftUI
+
+struct CombineTrainingJson: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CombineTrainingJson_Previews: PreviewProvider {
+    static var previews: some View {
+        CombineTrainingJson()
+    }
+}
