aff0724a8b28f54a886019ed2c6acc17c9c3cb2d
岡優志 <yu.j.0513@gmail.com>
2021-07-07 22:05:33 +0900

    FetchModelにエラーハンドリングを追加

--- OnboardViwe/OnboardViwe/Model/FetchUserRepository.swift
+++ OnboardViwe/OnboardViwe/Model/FetchUserRepository.swift
@@ -9,23 +9,32 @@ import Foundation
 import Combine
 
 protocol Fetcher {
-    func fetchUserRepository(query: String, completion: @escaping ([Item]) -> Void)
+    func fetchUserRepository(query: String, completion: @escaping (Result<[Item], APIError>) -> Void)
 }
 class FetchUserRepository: Fetcher {
-    func fetchUserRepository(query: String, completion: @escaping ([Item]) -> Void) {
-        guard let url: URL = URL(string: "https://api.github.com/search/repositories?q=\(query)") else { return print("URL Error") }
+    func fetchUserRepository(query: String, completion: @escaping (Result<[Item], APIError>) -> Void) {
+        guard let url: URL = URL(string: "https://api.github.com/search/repositories?q=\(query)")
+        else { return completion(.failure(.invalidURL)) }
         URLSession.shared.dataTask(with: url) { (data, response, error) in
-            guard let jsonData = data else { return print("Json Error") }
+            // response
+            guard let httpResponse = response as? HTTPURLResponse,
+                  (200...299).contains(httpResponse.statusCode)
+            else { return completion(.failure(.networkError)) }
             
             do {
+                guard let jsonData = data
+                else { return completion(.failure(.parserError)) }
                 let decoder = JSONDecoder()
                 decoder.keyDecodingStrategy = .convertFromSnakeCase
-                let repositories = try decoder.decode(Repositories.self, from: jsonData)
+                let object = try decoder.decode(Repositories.self, from: jsonData)
                 DispatchQueue.main.async {
-                    completion(repositories.items)
+                    completion(.success(object.items))
                 }
+                
             } catch {
-                print("items Decoder Error")
+                let urlSessionError = error
+                urlSessionError as? APIError == APIError.networkError ? completion(.failure(.networkError)) : completion(.failure(.unknown))
+                print("request failure: \(String(describing: urlSessionError))")
             }
         }
         .resume()
